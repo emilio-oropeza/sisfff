673c452d3d3c5a091c27993f67e85811479e8613
Emilio Oropeza <vocho17@gmail.com>
2018-08-01 22:15:38 -0500

    Subir ga

--- CreateUser.php
+++ CreateUser.php
@@ -1,11 +1,16 @@
 <?php
+include_once(dirname(__FILE__)."/config.inc.php");
+include_once(dirname(__FILE__)."/libraries/phpmailer/class.phpmailer.php");
+include_once(dirname(__FILE__)."/libraries/phpmailer/class.smtp.php");
+/*include_once("./config.inc.php");
+include_once("./libraries/phpmailer/class.phpmailer.php");
+include_once("./libraries/phpmailer/class.smtp.php");*/
+
+//$mail = "emilio.oropeza.palacios@gmail.com";
+$mail = "innovationawards@fff.fr";
+
 $json = file_get_contents('php://input');
 $respond = json_decode($json);
-
-$servername = "localhost";
-$username = "fffuser";
-$password = "fFfPaSsWoRd";
-$dbname = "fff";
 
 $company = $respond->company;
 $year = $respond->year;
@@ -13,24 +18,43 @@ $website = $respond->website;
 $name = $respond->name;
 $position = $respond->position;
 
-$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    echo "Error: Conexion fallida";
-} 
+$phpmail = new PHPMailer(true);
+$phpmail->isSendmail();
 
-$sql = "INSERT INTO companies (company_name, creation_year, website, name, position)
-VALUES ('".$company."', '".$year."', '".$website."', '".$name."', '".$position."')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+if(!empty($_smtpHost) && !empty($_smtpPort) && !empty($_smtpUser) && !empty($_smtpPass) && !empty($_smtpSecure)){
+    $phpmail->IsSMTP();
+    $phpmail->Host = $_smtpHost; 
+    $phpmail->Port = $_smtpPort;
+    $phpmail->Username = $_smtpUser;
+    $phpmail->Password = $_smtpPass;
+    $phpmail->SMTPAuth = $_smtpAuth;
+    $phpmail->SMTPSecure = $_smtpSecure;
 }
+try {
+    $phpmail->setFrom($_mailNoReplay, $_nameNoReplay);
+    $phpmail->addAddress($mail, $name." ".$company);
 
-$conn->close(); 
+    $phpmail->Subject = "Company ".$company;
 
-mysqli_close($conn);
+    $html = preg_replace('/[\\\\]/i', "", file_get_contents(dirname(__FILE__)."/template/register.html"));
+    $html = str_replace(
+        array("{company}", "{year}", "{website}", "{name}", "{position}"),
+        array($company, $year, $website, $name, $position),
+        $html
+    );
+
+    $phpmail->isHTML(true); 
+    //$phpmail->CharSet = 'UTF-8';
+    $phpmail->msgHTML($html);
+    $phpmail->AltBody = strip_tags($html);
+
+    if(!@$phpmail->send()){
+        echo 'Error '.$phpmail->ErrorInfo;
+    }else{
+        echo 'Send';
+    }
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
 ?>
---
+++ config.inc.php
@@ -0,0 +1,10 @@
+<?php
+$_mailNoReplay = "noreply@fff.fr";
+$_nameNoReplay = "Innovation Awards";
+
+$_smtpHost = "smtp.gmail.com";
+$_smtpPort = 587;
+$_smtpUser = "innovationawardssender@gmail.com";
+$_smtpPass = "9MiRrvhb4p";
+$_smtpAuth = true;
+$_smtpSecure = "tls"; //ssl o tlsinnovationawards@fff.fr
